cf518abafb8a3ba2432fc17538f0917454dba230
greenleafhub <eledora.2112@gmail.com>
2022-06-20 16:49:22 +0800

    Add search function

--- src/components/VGallery.vue
+++ src/components/VGallery.vue
@@ -17,17 +17,29 @@
 .text{
   text-align: center;
 }
+
+input[type=text] {
+  width: 250px;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  font-family: 'Cabin', sans-serif;
+  font-size: large;
+}
 </style>
 
 <template>
-
 <div class="text">
 <h1>🥔Chips🥔</h1>
 <p>Welcome to my chips review gallery!<br>Click on the photo to read my reviews✨</p>
 </div>
 
+<div class="text">
+    Search by tags: <input type="text" v-model="searchWords" placeholder="🔎try typing 'nice' here"><i class="fa fa-search"></i>
+    
+</div>
   <div class="gallery">
-    <div class="gallery-panel" v-for="photo in photos" :key="photo.code">
+    <div class="gallery-panel" v-for="photo in filterSearch" :key="photo.code">
       <router-link :to="`/photo/${photo.code}`">       
         <img :src="thumbUrl(photo.code)" />
       </router-link>
@@ -38,7 +50,6 @@
     <router-link to @click="moveUp">Back to top</router-link>
   </div>
 <br>
-
 </template>
 
 <script>
@@ -48,8 +59,7 @@ export default {
   name: "VGallery",
   data() {
     return {
-      search: "",
-      photos,
+      photos,  searchWords: ''
     };
   },
   methods: {
@@ -58,7 +68,15 @@ export default {
     },
      moveUp(){
            window.scrollTo(0,0);
+       
       },
+  },
+  computed:{
+    filterSearch() {   
+    return this.photos.filter(searchResult => {
+    searchResult.Tags = ""+searchResult.Tags      
+    return searchResult.Tags.includes(this.searchWords)});
+  }
   },
 };
 </script>
